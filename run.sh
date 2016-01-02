fb2f2be121acc4b9bcefb1d6b61da0e956fcbd79
Oren <orengolan@gmail.com>
2016-01-02 15:33:28 -0800

    misc

--- run.sh
+++ run.sh
@@ -1,20 +1,12 @@
-# test-disque
+#/bin/sh
 
-Get the binaries
-```
-https://github.com/antirez/disque
-cd disque
-make
-```
-
-Run 3 nodes
-```
 export PATH=$PATH:~/projects/disque/disque/src
+
 cd node1
 disque-server $(pwd)/disque.conf
+cd ..
 cd node2
 disque-server $(pwd)/disque.conf
+cd ..
 cd node3
 disque-server $(pwd)/disque.conf
-```
-
